b46f11d3f59aecd964d8b8c709cfcfadd984c776
ramanboff <bayramofframan@gmail.com>
2023-10-12 12:18:53 +0400

    step button changed to range input

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,27 @@ function Counter() {
   console.log(date);
   date.setDate(date.getDate() + count);
 
+
+  const handleReset = () => {
+    setStep(1)
+    setCount(0)
+  }
+
   return (
     <div>
       <div>
-        <button onClick={() => setStep(step - 1)}>-</button>
+        {/* <button onClick={() => setStep(step - 1)}>-</button>
         <span>Step:{step}</span>
-        <button onClick={() => setStep(step + 1)}>+</button>
+        <button onClick={() => setStep(step + 1)}>+</button> evezine  altdaki input */}
+        <input type="range" min="0" max="10" value={step} onChange={(e) => setStep(Number(e.target.value))}/>
+        <span>Step:{step}</span>
       </div>
       <div>
         <button onClick={() =>  setCount((a) => a -  step)}>-</button>
-        <span>Count:{count}</span>
+        <span>
+          {/* Count:{count} */}
+          <input type="text" value={count} onChange={(e) => setCount(e.target.value)} />
+          </span>
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
       <p>
@@ -39,6 +50,12 @@ function Counter() {
         </span>
         <span> {date.toDateString()}</span>
       </p>
+      <div>
+        {count !== 0 || step !== 1 ? (
+ <button onClick={handleReset}>Reset</button>
+        ): null}
+       
+      </div>
     </div>
   );
 }
